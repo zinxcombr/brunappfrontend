3dbf573de659662c625cd0bfe47ade4e186b8c4d
zinxcombr <bruno@zinx.com.br>
2020-07-01 09:26:22 -0300

    organizacao de codigo

---
+++ vendor/custom/data-editable.js
@@ -0,0 +1,32 @@
+
+    $('body').on('click', '[data-editable]', function(){
+  
+  var $el = $(this);
+              
+  var $input = $('<input/>').val( $el.text() );
+  $el.replaceWith( $input );
+  
+  var save = function(){
+    var $p = $('<p data-editable />').text( $input.val() );
+    $input.replaceWith( $p );
+  };
+  
+  $input.one('blur', save).focus();
+  
+});
+
+
+$('body').on('click', '[data-editable-2]', function(){
+  
+  var $el = $(this);
+              
+  var $input = $('<textarea/>').val( $el.text() );
+  $el.replaceWith( $input );
+  
+  var save = function(){
+    var $p = $('<p data-editable />').text( $input.val() );
+    $input.replaceWith( $p );
+  };
+  
+  $input.one('blur', save).focus();
+  });
